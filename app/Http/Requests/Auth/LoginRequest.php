dfe51b146599f2c545540162e84f7edf57cd146b
HieuVo <votrunghieu236@gmail.com>
2025-03-30 12:31:24 +0700

    Login,Logout, Register

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use function trans;
 
 class LoginRequest extends FormRequest
 {
